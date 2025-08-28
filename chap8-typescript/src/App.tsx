2b0c73df6e53f42c9578c50fe969da1ee1792953
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-28 18:02:31 +0900

    事前準備

--- chap8-typescript/src/App.tsx
+++ chap8-typescript/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { ListItem } from "./components/ListItem";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  // 取得したユーザー情報
+  const [users, setUsers] = useState([]);
+
+  // 画面表示時にユーザー情報取得
+  // ※実際にはこのエンドポイントは存在しないので注意
+  useEffect(() => {
+    axios.get<User[]>("https://example.com/users").then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      {users.map((user) => (
+        <ListItem
+          id={user.id}
+          name={user.nama}
+          age={user.age}
+        />
+      ))}
+    </div>
+  );
+};
