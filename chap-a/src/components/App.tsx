b3ce4d01706858d248ec4544d0ccd4ebf364d6f8
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-30 14:53:33 +0900

    コンポーネント化
      - onClickDelete 関数は Props で渡していくことになるので useCallback を使って関数をメモ化しておく

--- chap-a/src/components/App.tsx
+++ chap-a/src/components/App.tsx
@@ -1,6 +1,8 @@
 import type { ChangeEvent, FC } from "react"; // 教本と違って FC ではなく　　 type FC と書くようだ
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { styled } from "styled-components";
+
+import { MemoList } from "./MemoList";
 
 
 export const App: FC = () => {
@@ -23,44 +25,26 @@ export const App: FC = () => {
   };
 
   // [削除]ボタン押下時（何番目が押されたかを引数で受け取る）
-  const onClickDelete = (index: number) => {
+  const onClickDelete = useCallback((index: number) => {
     // State 変更を正常に検出させるため新たな配列を生成
     const newMemos = [...memos];
     // メモ配列から該当の要素を削除
     newMemos.splice(index, 1);
     setMemos(newMemos);
-  };
+  }, [memos]);
 
   return(
     <div>
       <h1>簡単メモアプリ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
-      <SContainer>
-        <ul>
-          {memos.map((memo, index) => (
-            <li key={memo}>
-              <SMemoWrapper>
-                <p>{memo}</p>
-                <SButton onClick={() => {onClickDelete(index)}}>削除</SButton>
-              </SMemoWrapper>
-            </li>
-          ))}
-        </ul>
-      </SContainer>
+      <MemoList memos={memos} onClickDelete={onClickDelete} />
+
+
     </div>
   );
 };
 
 const SButton = styled.button`
   margin-left: 16px;
-`;
-const SContainer = styled.div`
-  border: solid 1px #ccc;
-  paddig: 16px;
-  margin: 8px;
-`;
-const SMemoWrapper = styled.div`
-  display: flex;
-  aligin-items: center;
 `;
